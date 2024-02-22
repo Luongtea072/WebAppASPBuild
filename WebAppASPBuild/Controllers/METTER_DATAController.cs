95ddd6c6e84553c0a866e1cdcb861334dab76161
TraTo <minhti195@gmail.com>
2024-02-22 11:39:28 +0700

    deleted some unused files and fix the small graph not showing bugs by Kiet 22.2.2024

--- WebAppASPBuild/Controllers/METTER_DATAController.cs
+++ WebAppASPBuild/Controllers/METTER_DATAController.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
-using aueProject;
-using Newtonsoft.Json;
 
 namespace aueProject.Controllers
 {
-    public class METTER_DATAController : Controller
+	public class METTER_DATAController : Controller
     {
         private Entities db = new Entities();
 
